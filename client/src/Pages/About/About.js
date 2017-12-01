5d53e6ca907ff1bda519bb51f433b3e823926374
fathao <zihaozhen@gmail.com>
2017-11-30 16:14:05 -0800

    starting on about page bootstrap

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Hero from "../../Components/Hero";
+import "./About.css";
 
 const About = () =>
   <div>
-     <Hero backgroundImage="https://images.crutchfieldonline.com/ImageHandler/scale/978/978/products/2014/41/158/x158NWZA17S-o_lifestyle.jpg">
+     <Hero className="hero" backgroundImage="https://images.crutchfieldonline.com/ImageHandler/scale/978/978/products/2014/41/158/x158NWZA17S-o_lifestyle.jpg">
       <h1>Twilio Text</h1>
       <h2>ABOUT US</h2>
     </Hero>
-    <p>nsjdkndsjk</p>
+
+    <row>
 
 
 
+      
+    </row>
   </div>;
 
 export default About;
