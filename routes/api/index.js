b37237944ab155bbeea173e70593383faef04412
Winfred Sunga <winfredsunga01@gmail.com>
2017-11-30 23:01:59 -0800

    started server side api routes

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router(); //use express Router
+const userAndApptRoutes = require("./userAndAppt");
+
+//user and appointment router
+router.use("/", userAndApptRoutes)
+
+module.exports = router;
