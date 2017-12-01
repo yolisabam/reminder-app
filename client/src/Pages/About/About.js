734307e2076ee9b44d77c7cc909fa559fe46b51e
Yolisa Bam <yolisabam@calvisitor-10-105-168-71.calvisitor.1918.berkeley.edu>
2017-11-30 16:12:45 -0800

    committing changes

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -3,7 +3,7 @@ import Hero from "../../Components/Hero";
 
 const About = () =>
   <div>
-     <Hero backgroundImage="https://images.crutchfieldonline.com/ImageHandler/scale/978/978/products/2014/41/158/x158NWZA17S-o_lifestyle.jpg">
+     <Hero backgroundImage="https://images.crutchfieldonline.com/ImageHandler/scale/978/978/products/2014/41/158/x158NWZA17S-o_lifestyle.jpg" >
       <h1>Twilio Text</h1>
       <h2>ABOUT US</h2>
     </Hero>
