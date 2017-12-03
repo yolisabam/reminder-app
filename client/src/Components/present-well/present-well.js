92966faa2829588efe603d86f7d290e7b4b14a63
fathao <zihaozhen@gmail.com>
2017-12-02 17:50:18 -0800

    added more on present-well

--- client/src/Components/present-well/present-well.js
+++ client/src/Components/present-well/present-well.js
@@ -5,12 +5,13 @@ const PresentWell = props => {
   return (
     <div>
       <div className="panel panel-default" id="panel">
-        {props.results.map(res =>
+        {props.upcomingAppts.map(res =>
           <div className="panel-body" key={res.title}>
-            <h5>{res.appointmentName}</h5>
-            <h5>{res.date}</h5>
-            <h5>{res.time}</h5>
-            <h5>{res.notification}</h5>
+            <h5>{res.apptName}</h5>
+            <h5>{res.apptNotificationNumber}</h5>
+            <h5>{res.apptDate}</h5>
+            <h5>{res.apptTime}</h5>
+            <h5>{res.Notification}</h5>
             <button className="btn btn-default">Update</button>
             <button className="btn btn-default">Delete</button>
           </div>
