fb6f61b4ca13c21bdbf9a8807b2f52ee398e1bb4
fathao <zihaozhen@gmail.com>
2017-12-03 22:40:26 -0800

    Add user ref to appointment model

--- models/appointment.js
+++ models/appointment.js
@@ -3,10 +3,11 @@ const moment = require('moment');
 const Twilio = require('twilio');
 //const config = require('../config')
 
-const Schema = mongoose.Schema;
-
-
-const appointmentSchema = new Schema({
+const appointmentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
   appointmentName: {
     type: String,
     required: true
@@ -81,4 +82,3 @@ const appointmentSchema = new Schema({
 
 const Appointment = mongoose.model("Appointment", appointmentSchema);
 module.exports = Appointment;
-
