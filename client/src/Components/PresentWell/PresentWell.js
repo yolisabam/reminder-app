103cddac840b3b5261992e1dc2513539e44b6706
fathao <zihaozhen@gmail.com>
2017-12-03 23:46:46 -0800

    Render appointment lists

--- client/src/Components/PresentWell/PresentWell.js
+++ client/src/Components/PresentWell/PresentWell.js
@@ -1,39 +1,60 @@
 import React, { Component } from "react";
 import API from "../../Utils/api";
 
+const Appointment = ({ appts, handleUpdate, handleDelete }) => (
+  appts.map(appt =>
+    <div className="panel-body" key={appt._id}>
+      <h5>Appointment Name: {appt.appointmentName}</h5>
+      <h5>Appointment Number: {appt.appointmentNumber}</h5>
+      <h5>Date: {appt.date}</h5>
+      <h5>Time: {appt.time}</h5>
+      <h5># of Notification: {appt.notification}</h5>
+      <button className="btn btn-default" onClick={handleUpdate}>Update</button>
+      <button className="btn btn-default" onClick={handleDelete}>Delete</button>
+    </div>
+  )
+);
+
 class PresentWell extends Component {
   state = {
     appointments: []
   };
 
   componentWillMount() {
-    API.getUserAppointments(this.props.user._id)
-      .then(res => {
-        console.log('res', res);
-      })
-      .catch(err => console.error(err));
+    this.loadAppointments();
   }
 
   render() {
-    // const { upcomingAppts } = this.state;
+    const upcomingAppts = this.getUpcomingAppointments();
+    const pastAppts = this.getPastAppointments();
     return (
       <div>
         PresentWell
         <div className="panel panel-default" id="panel">
-          {/* {upcomingAppts.map(res =>
-            <div className="panel-body" key={res.title}>
-              <h5>{res.apptName}</h5>
-              <h5>{res.apptNotificationNumber}</h5>
-              <h5>{res.apptDate}</h5>
-              <h5>{res.apptTime}</h5>
-              <h5>{res.Notification}</h5>
-              <button className="btn btn-default">Update</button>
-              <button className="btn btn-default">Delete</button>
-            </div>
-          )} */}
+          <Appointment appts={upcomingAppts} />
+        </div>
+        Past Well
+        <div className="panel panel-default" id="panel">
+          <Appointment appts={pastAppts} />
         </div>
       </div>
     );
+  }
+
+  loadAppointments() {
+    API.getUserAppointments(this.props.user._id)
+      .then(res => {
+        this.setState({ appointments: res.data });
+      })
+      .catch(err => console.error(err));
+  }
+
+  getUpcomingAppointments() {
+    return this.state.appointments.filter(appt => appt.notification > 0);
+  }
+
+  getPastAppointments() {
+    return this.state.appointments.filter(appt => appt.notification <= 0);
   }
 }
 
