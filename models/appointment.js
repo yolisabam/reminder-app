c8ce8d54d8bd714564bed440d1747efade30d946
Winfred Sunga <winfredsunga01@gmail.com>
2017-12-04 11:40:14 -0800

    fixed typos

--- models/appointment.js
+++ models/appointment.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
 const Twilio = require('twilio');
-//const config = require('../config')
+const config = require('../config')
 
 const Schema = mongoose.Schema;
 
@@ -11,7 +11,7 @@ const appointmentSchema = new Schema({
     type: String,
     required: true
   },
-  date: {
+  time: {
     type: Date,
     required : true
     // validate: function (v) {
@@ -36,48 +36,48 @@ const appointmentSchema = new Schema({
   }
 }); 
 
-// appointmentSchema.methods.requiresNotification = function(date) {
-//   return Math.round(moment.duration(moment(this.time))
-//                           .diff(moment(date))
-//                           .asMinutes()) === this.notification;
-// };
+appointmentSchema.methods.requiresNotification = function(date) {
+  return Math.round(moment.duration(moment(this.time))
+                          .diff(moment(date))
+                          .asMinutes()) === this.notification;
+};
 
-// appointmentSchema.statics.sendNotifications = function(cb) {
-//   const searchDate = new Date();
-//   Appointment
-//     .find()
-//     .then(function(appointments) {
-//       appointments = appointments.filter(function(appointment){
-//         return appointment.requiresNotification(searchDate);
-//       });
-//       if (appointments.length > 0) {
-//         sendNotifications(appointment);
-//       }
-//     });
+appointmentSchema.statics.sendNotifications = function(cb) {
+  const searchDate = new Date();
+  Appointment
+    .find()
+    .then(function(appointments) {
+      appointments = appointments.filter(function(appointment){
+        return appointment.requiresNotification(searchDate);
+      });
+      if (appointments.length > 0) {
+        sendNotifications(appointment);
+      }
+    });
 
-//     function sendNotifications(appointments) {
-//       const client = new Twilio(config.twilioAccountSid, config.twilioAuthToken);
-//       appointments.forEach(function(appointment) {
-//         const message = {
-//           to: `+ ${appointment.appointmentNumber}`,
-//           from: config.twilioPhoneNumber,
-//           body: `Hi! Just a quick reminder that ${appointment.appointmentName} is coming up !`,
-//         };
+    function sendNotifications(appointments) {
+      const client = new Twilio(config.twilioAccountSid, config.twilioAuthToken);
+      appointments.forEach(function(appointment) {
+        const message = {
+          to: `+ ${appointment.appointmentNumber}`,
+          from: config.twilioPhoneNumber,
+          body: `Hi! Just a quick reminder that ${appointment.appointmentName} is coming up !`,
+        };
 
-//         client.messages.create(message, function(err, res){
-//           if(err) {
-//             console.log(err);
-//           } else {
-//             let phoneNumber = appointment.appointmentNumber;
-//             console.log(`Reminder sent to ${phoneNumber}`);
-//           }
-//         });
-//       });
-//       if(cb) {
-//         cb.call();
-//       }
-//     }
-// };
+        client.messages.create(message, function(err, res){
+          if(err) {
+            console.log(err);
+          } else {
+            let phoneNumber = appointment.appointmentNumber;
+            console.log(`Reminder sent to ${phoneNumber}`);
+          }
+        });
+      });
+      if(cb) {
+        cb.call();
+      }
+    }
+};
 
 const Appointment = mongoose.model("Appointment", appointmentSchema);
 module.exports = Appointment;
