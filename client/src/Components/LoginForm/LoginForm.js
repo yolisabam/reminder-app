445f5e44a702e35781aedc4719797e8092fdda24
fathao <zihaozhen@gmail.com>
2017-12-06 11:00:09 -0800

    added some logic for the login form, but not fully functional yet

--- client/src/Components/LoginForm/LoginForm.js
+++ client/src/Components/LoginForm/LoginForm.js
@@ -316,9 +316,6 @@ class LoginForm extends Component {
           <div className="panel-body">
             <form className="form-horizontal">  
               <div className="form-group">
-<<<<<<< HEAD
-                <label htmlFor="input-email" className="col-sm-4 control-label">Email</label>
-=======
                 <label htmlFor="input-email" className="col-sm-4 control-label animated bounceInLeft">Email</label>
                  <label>Test</label>
                 <div className="col-sm-6">
@@ -338,7 +335,6 @@ class LoginForm extends Component {
                 </div> 
 
                 <label htmlFor="inputUserName" className="col-sm-4 control-label">Email</label>
->>>>>>> master
                 <div className="col-sm-6">
                   <input 
                     value={this.state.loginEmail}
@@ -400,7 +396,29 @@ class LoginForm extends Component {
       <form id="form" className="topBefore">
           
         <input id="name" type="email" placeholder="email" value={this.state.loginEmail} name="loginEmail" onChange={this.handleInputChange} type="text" id="input-email" ></input>
+          {this.state.isLoginEmailEmpty &&
+            <div id="error-email-left-empty">
+              <p className="error text-center">Please enter your email address!</p>
+            </div>
+          }
+          
+          {!this.state.isValidEmail && 
+            <div id="error-username-not-exist">
+              <p className="error text-center">This email does not exist!</p>
+            </div>
+          }
         <input id="email" type="password" placeholder="password" value={this.state.loginPassword} name="loginPassword" onChange={this.handleInputChange} type="password" id="input-password"></input>
+
+          {this.state.isLoginPasswordEmpty &&
+            <div id="error-password-left-empty">
+              <p className="error text-center">Please enter your password!</p>
+            </div>
+          }
+          {!this.state.isValidPassword &&
+            <div id="error-password-incorrect">
+              <p className="error text-center">The password is incorrect!</p>
+            </div>
+          }
         <input id="submit" type="submit" value="Sign In!" onClick={this.handleLoginFormSubmit}></input>
       </form>
         <div className="col-sm-offset-2 col-sm-5">
