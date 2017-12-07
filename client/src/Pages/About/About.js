fbabc35f534385ca30256aadd339eb3dda2a1a83
Yolisa Bam <yolisabam@Yolisas-MacBook-Pro.local>
2017-12-07 09:07:55 -0800

    adding pics to about page

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -62,25 +62,28 @@ const About = () =>
         <div className="caption">
           <p>Yolisa</p>
         </div>
-        <a href="http://yolisa-bam.com/"><img className="img-circle" img width="200px" height="200px" src="https://ginnodangan.files.wordpress.com/2011/04/gintamaop9-02.jpg" alt="Cinque Terre"></img></a>
+        <a href="http://yolisa-bam.com/"><img className="img-circle" img width="200px" height="200px" src="/assets/yolisa.jpg" alt="Cinque Terre"></img></a>
       </div>
       <div className="col-md-3">
         <div className="caption">
           <p>Winfred</p>
         </div>
-        <a href="http://winfredsunga.com"><img className="img-circle" img width="200px" height="200px" src="https://ginnodangan.files.wordpress.com/2011/04/gintamaop9-03.jpg" alt="Cinque Terre"></img></a>
+        <a href="http://winfredsunga.com"><img className="img-circle" img width="200px" height="200px" src="/assets/WinfredSungaPic.jpg" alt="Cinque Terre"></img></a>
       </div>
       <div className="col-md-3">
         <div className="caption">
           <p>James</p>
         </div>
-        <a href="http://jamesreantillo.com/portfolio/"><img className="img-circle" img width="200px" height="200px" src="http://i0.kym-cdn.com/photos/images/original/000/427/974/846.png" alt="Cinque Terre"></img></a>
+        <a href="http://jamesreantillo.com/portfolio/"><img className="img-circle" img width="200px" height="200px" src="/assets/James.JPG" alt="Cinque Terre"></img></a>
+        <div className="caption">
+          <p>James</p>
+        </div>
       </div>
       <div className="col-md-3">
-        <div className="caption">
+        <a href="https://fathao.github.io/Alex-Z/"><img className="img-circle" img width="200px" height="200px" src="/assets/Alex.jpg" alt="Cinque Terre"></img></a>
+         <div className="caption">
           <p>Alex</p>
         </div>
-        <a href="https://fathao.github.io/Alex-Z/"><img className="img-circle" img width="200px" height="200px" src="http://wfiles.brothersoft.com/g/gintoki_79205-1600x1200.jpg" alt="Cinque Terre"></img></a>
       </div>
 
      </div>
